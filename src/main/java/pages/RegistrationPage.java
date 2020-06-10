1e4cd25a97ea224c54803d9521d24ae121c76173
Anup Kapale <anup.k@turtlemint.com>
2020-06-10 18:11:40 +0530

    Registration pages and test cases added

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage {
@@ -10,5 +12,14 @@ public class RegistrationPage {
     public RegistrationPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
+    }
+
+
+    @FindBy(xpath = "//h1[@class='reg-frm-head--txt']")
+    private WebElement headerTitle;
+
+
+    public String getheaderTitle(){
+        return headerTitle.getText();
     }
 }
