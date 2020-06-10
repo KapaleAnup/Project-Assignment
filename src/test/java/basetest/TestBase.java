cc31535cb2a210d7f4eba63c9b0f7678c45029bb
Anup Kapale <anup.k@turtlemint.com>
2020-06-10 17:50:09 +0530

    testbase created

---
+++ src/test/java/basetest/TestBase.java
@@ -0,0 +1,87 @@
+package basetest;
+
+import com.google.common.io.Files;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
+
+public class TestBase {
+
+    private static WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    private void setDriver(String browserType, String appURL) {
+        switch (browserType) {
+            case "chrome":
+                driver = initChromeDriver(appURL);
+                break;
+            case "firefox":
+                driver = initFirefoxDriver(appURL);
+                break;
+            default:
+                System.out.println("browser : " + browserType + " is invalid, Launching Firefox as browser of choice..");
+                driver = initChromeDriver(appURL);
+        }
+    }
+
+    private static WebDriver initChromeDriver(String appURL) {
+        System.out.println("Launching google chrome with new profile..");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to(appURL);
+        return driver;
+    }
+
+    private static WebDriver initFirefoxDriver(String appURL) {
+        System.out.println("Launching Firefox browser..");
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.navigate().to(appURL);
+        return driver;
+    }
+
+    @Parameters({ "browserType", "appURL" })
+    @BeforeClass
+    public void initializeTestBaseSetup(String browserType, String appURL) {
+        try {
+            setDriver(browserType, appURL);
+
+        } catch (Exception e) {
+            System.out.println("Error....." + e.getStackTrace());
+        }
+    }
+
+
+    @BeforeMethod
+    public void failedTestCasesScreenshots(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            TakesScreenshot image = (TakesScreenshot) driver;
+            File screenshot = image.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot,new File("resources/screenshots/" + result.getName() + ".png"));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
